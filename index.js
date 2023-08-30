8c46db3f6ee5b1a65c71629942b553816c2b9fee
GYAN RANJAN MOHANTY <81802413+gyanranjan17@users.noreply.github.com>
2023-08-31 00:34:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+function generateRandomTestCases() {
+    const gridSize = 9;
+    const sudokuGrid = document.getElementById("sudoku-grid");
+
+    for (let row = 0; row < gridSize; row++) {
+        for (let col = 0; col < gridSize; col++) {
+            const cellId = `cell-${row}-${col}`;
+            const cell = document.getElementById(cellId);
+
+            // Generate a random number between 1 and 9 for each cell
+            const randomValue = Math.floor(Math.random() * 9) + 1;
+            cell.value = randomValue;
+        }
+    }
+}
 document.addEventListener('DOMContentLoaded', function () {
     const gridSize = 9;
+    const res_btn=document.getElementById("reset-btn");
     const solveButton = document.getElementById("solve-btn");
     solveButton.addEventListener('click', solveSudoku);
 
-    const sudokuGrid = document.getElementById("sudoku-grid");
+    let sudokuGrid = document.getElementById("sudoku-grid");
     // Create the sudoku grid and input cells
+    res_btn.addEventListener('click', function(){
+        location.reload();
+    });
     for (let row = 0; row < gridSize; row++) {
         const newRow = document.createElement("tr");
         for (let col = 0; col < gridSize; col++) {
@@ -30,17 +49,32 @@ async function solveSudoku() {
         for (let col = 0; col < gridSize; col++) {
             const cellId = `cell-${row}-${col}`;
             const cellValue = document.getElementById(cellId).value;
-            sudokuArray[row][col] = cellValue !== "" ? parseInt(cellValue) : 0;
+            if (cellValue === "" || (parseInt(cellValue) >= 1 && parseInt(cellValue) <= 9)) {
+                sudokuArray[row][col] = cellValue === "" ? '.' : parseInt(cellValue);
+            } else {
+                alert("Invalid input. Please enter numbers from 1 to 9.");
+                return; // Exit the function
+            }
         }
     }
 
+
+    // Check if the input Sudoku is valid
+    console.log(sudokuArray);
+    var chk=isValidSudoku(sudokuArray);
+    if (!chk) {
+        console.log(chk);
+        alert("The input Sudoku puzzle is not valid.");
+        return;
+    }
+    
     // Identify user-input cells and mark them
     for (let row = 0; row < gridSize; row++) {
         for (let col = 0; col < gridSize; col++) {
             const cellId = `cell-${row}-${col}`;
             const cell = document.getElementById(cellId);
 
-            if (sudokuArray[row][col] !== 0) {
+            if (sudokuArray[row][col] !== '.') {
                 cell.classList.add("user-input");
             }
         }
@@ -66,12 +100,58 @@ async function solveSudoku() {
     }
 }
 
+// Rest of the code (solveSudokuHelper, isValidMove, sleep) remains the same
+function isValidSudoku(board) {
+    const gridSize = 9;
+
+    // Check rows and columns for duplicates
+    for (let i = 0; i < gridSize; i++) {
+        const rowSet = new Set();
+        const colSet = new Set();
+
+        for (let j = 0; j < gridSize; j++) {
+            const rowVal = board[i][j];
+            const colVal = board[j][i];
+
+            if (rowVal !== "." && rowSet.has(rowVal)) {
+                return false;
+            }
+            rowSet.add(rowVal);
+
+            if (colVal !== "." && colSet.has(colVal)) {
+                return false;
+            }
+            colSet.add(colVal);
+        }
+    }
+
+    // Check subgrids for duplicates
+    for (let i = 0; i < gridSize; i += 3) {
+        for (let j = 0; j < gridSize; j += 3) {
+            const subgridSet = new Set();
+
+            for (let r = i; r < i + 3; r++) {
+                for (let c = j; c < j + 3; c++) {
+                    const subgridVal = board[r][c];
+
+                    if (subgridVal !== "." && subgridSet.has(subgridVal)) {
+                        return false;
+                    }
+                    subgridSet.add(subgridVal);
+                }
+            }
+        }
+    }
+
+    return true; // If all checks pass, the Sudoku is valid
+}
+
 function solveSudokuHelper(board) {
     const gridSize = 9;
 
     for (let row = 0; row < gridSize; row++) {
         for (let col = 0; col < gridSize; col++) {
-            if (board[row][col] === 0) {
+            if (board[row][col] === '.') {
                 for (let num = 1; num <= 9; num++) {
                     if (isValidMove(board, row, col, num)) {
                         board[row][col] = num;
@@ -81,7 +161,7 @@ function solveSudokuHelper(board) {
                             return true; // Puzzle solved
                         }
 
-                        board[row][col] = 0; // Backtrack
+                        board[row][col] = '.'; // Backtrack
                     }
                 }
                 return false; // No valid number found
@@ -120,3 +200,12 @@ function isValidMove(board, row, col, num) {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+
+
+
+
+
+
+
